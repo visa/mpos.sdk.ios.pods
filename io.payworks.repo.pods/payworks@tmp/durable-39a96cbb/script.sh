3483ac352080e03603d76465013fb2bf16e4b766
Mister Jenkins <Jenkins@payworks.com>
2019-08-13 09:45:14 +0200

    [Jenkins] Push new version 2.40.0

---
+++ io.payworks.repo.pods/payworks@tmp/durable-39a96cbb/script.sh
@@ -0,0 +1,5 @@
+
+                            git add --all
+                            git commit -am "[Jenkins] Push new version 2.40.0"
+                            git push origin master
+                        
