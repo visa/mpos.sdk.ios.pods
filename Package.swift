821ebf5d48fb945ef56272fc02d2e12184a1aede
aleix guri <guria@visa.com>
2022-10-24 13:13:55 +0200

    SPM-2.55.0

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mpos_core",
+    platforms: [
+        .iOS(.v15)
+       ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "mpos_core",
+                 targets: ["mpos_core"]),
+    ],
+    targets: [
+        .binaryTarget(
+                    name: "mpos_core",
+                    url:"https://artifactory.trusted.visa.com:443/mpos-releases/io/payworks/mpos.ios.sdk/2.55.0/mpos.ios.sdk-2.55.0.zip",
+                    checksum: "16af7011f9f1db60a681cffe27a0e560d52d5e27edd16f7fa56980138930ffce"
+            ),
+    ]
+)
+
